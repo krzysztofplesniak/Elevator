945ec2e8b908884e7a72c536ee6aa84d0e283593
krzysztof plesniak <krzysztofplesniak@interia.pl>
2020-07-14 12:11:58 +0200

    Make build production version

---
+++ React/build/precache-manifest.b6210b44e8fb7390f0ca997bbdeec183.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09e7f53f3520128fb13d1a7d0f63c99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b47fa64583e85439c6cb",
+    "url": "/static/css/main.3ec5e7b0.chunk.css"
+  },
+  {
+    "revision": "ee45629547a0b8375ef7",
+    "url": "/static/js/2.d0aa4472.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0aa4472.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47fa64583e85439c6cb",
+    "url": "/static/js/main.281394a1.chunk.js"
+  },
+  {
+    "revision": "50a3ceb2cff5463c61b5",
+    "url": "/static/js/runtime-main.ffe76a0c.js"
+  },
+  {
+    "revision": "98e4128bfa4be8ff2cb352c360dd06c9",
+    "url": "/static/media/skytower.98e4128b.jpg"
+  }
+]);
